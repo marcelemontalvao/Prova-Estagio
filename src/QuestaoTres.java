d97b6c7f650c06ae8c011733635647d11a25907d
Marcele Montalvão <99314176+marcelemontalvao@users.noreply.github.com>
2022-06-05 18:26:04 -0300

    Delete QuestaoTres.java

--- src/QuestaoTres.java
+++
@@ -1,4 +0,0 @@
-public class QuestaoTres {
-    //3. O time de desenvolvimento analisou e descobriu que o projeto está gerando arquivos pesados e desnecessários para serem comitados no repositório git. Em qual arquivo você deve incluir a lista de extensões para que esses arquivos desnecessários sejam desconsiderados?
-        //.gitignore
-}
