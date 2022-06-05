15c5a5e0d7709e1ed2e3050487fa25c5e5873466
Marcele Montalvão <99314176+marcelemontalvao@users.noreply.github.com>
2022-06-05 18:25:45 -0300

    Delete QuestaoQuatro.java

--- src/QuestaoQuatro.java
+++
@@ -1,4 +0,0 @@
-public class QuestaoQuatro {
-    //4. Com suas palavras, descreva as vantagens de utilizar o Scrum no seu projeto.
-        // Com o Scrum é possível adaptar o projeto de acordo com as necessidades que vão surgindo ao decorrer do tempo. Além disso, com o Scrum é possível acompanhar o progresso das atividades de cada um do grupo e assim, torna possível também a entrega de feedbacks de maneira melhor e mais contínua. Tudo isso contribui para a melhora do projeto final.
-}
