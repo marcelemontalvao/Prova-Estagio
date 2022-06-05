66806bbdda7543c578253925dc9ca59c15629c3d
Marcele Montalvão <99314176+marcelemontalvao@users.noreply.github.com>
2022-06-05 18:26:24 -0300

    Delete QuestaoUm.java

--- src/QuestãoUm.java
+++
@@ -1,7 +0,0 @@
-public class QuestãoUm {
-    //1. Baseado no Scrum Guide, qual o tempo máximo deveriam ter as seguintes cerimônias:
-        //a. Daily: deve durar no máximo 15 minutos.
-        //b. Planning: deve durar no máximo 8 horas para a sprint de um mês.
-        //c. Review: No máximo 4 horas para uma sprint de um mês.
-        //d. Retrospectiva: No máximo 3h para uma sprint de um mês.
-}
