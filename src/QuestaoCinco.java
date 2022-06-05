e4ede20203c0dc223b8a2d60547b57cdd8b0fdc5
Marcele Montalvão <99314176+marcelemontalvao@users.noreply.github.com>
2022-06-05 18:24:42 -0300

    Delete QuestaoCinco.java

--- src/QuestaoCinco.java
+++
@@ -1,4 +0,0 @@
-public class QuestaoCinco {
-    //5. José foi promovido de seu cargo anterior e se tornou o novo Product Owner do time, quais são suas novas responsabilidades como PO?
-        //Agora ele precisa maximizar o valor do produto resultante do trabalho do time scrum através do gerenciamento eficaz do backlog do produto.
-}
